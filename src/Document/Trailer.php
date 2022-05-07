dd04b99df8112fe26db4bc31808b2437e78c6eb0
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-05-07 21:58:07 +0200

    Add documentation

--- src/Document/Trailer.php
+++ src/Document/Trailer.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 namespace PrinsFrank\PdfParser\Document;
 
+/**
+ * PDF 32000-1:2008
+ * The trailer of a PDF file enables a conforming reader to quickly find the cross-reference table and certain special objects
+ */
 final class Trailer
 {
     private readonly Document $document;
--- src/Parser/Section/TrailerSectionParser.php
+++ src/Parser/Section/TrailerSectionParser.php
@@ -9,6 +9,15 @@ use PrinsFrank\PdfParser\Enum\Marker;
 use PrinsFrank\PdfParser\Exception\MarkerNotFoundException;
 use PrinsFrank\PdfParser\Exception\ParseFailureException;
 
+/**
+ * PDF 32000-1:2008
+ * Conforming readers should read a PDF file from its end. The last line of the file shall contain only the end-of-file
+ * marker, %%EOF. The two preceding lines shall contain, one per line and in order, the keyword startxref and the byte
+ * offset in the decoded stream from the beginning of the file to the beginning of the xref keyword in the last
+ * cross-reference section. The startxref line shall be preceded by the trailer dictionary, consisting of the keyword
+ * trailer followed by a series of key-value pairs enclosed in double angle brackets (<<...>>) (using LESS-THAN SIGNs
+ * (3ch) and GREATER-THAN SIGNs (3Eh)).
+ */
 class TrailerSectionParser implements SectionParser
 {
     /**
@@ -33,7 +42,7 @@ class TrailerSectionParser implements SectionParser
 
         $byteOffsetLastCrossReferenceSection = substr($document->content, $startXrefMarkerPos + strlen(Marker::START_XREF->value),  -($document->fileLength - $eofMarkerPos));
         if ($byteOffsetLastCrossReferenceSection === false) {
-            throw new ParseFailureException('Failed to retrieve the byte offset for the last cross reference section. Document lenght: "' . $document->fileLength . '", eof marker pos: "' . $eofMarkerPos . '"');
+            throw new ParseFailureException('Failed to retrieve the byte offset for the last cross reference section. Document length: "' . $document->fileLength . '", eof marker pos: "' . $eofMarkerPos . '"');
         }
         $trailer->setByteOffsetLastCrossReferenceSection((int) $byteOffsetLastCrossReferenceSection);
 
