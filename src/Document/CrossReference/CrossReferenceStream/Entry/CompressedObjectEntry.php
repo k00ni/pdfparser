b24d456c8d97168fdce0ee39e4bc1cdc20b1b19b
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-10-22 20:30:42 +0200

    Add implicit generation number to compressed object entry

--- src/Document/CrossReference/CrossReferenceStream/Entry/CompressedObjectEntry.php
+++ src/Document/CrossReference/CrossReferenceStream/Entry/CompressedObjectEntry.php
@@ -4,6 +4,15 @@ namespace PrinsFrank\PdfParser\Document\CrossReference\CrossReferenceStream\Entr
 
 class CompressedObjectEntry
 {
+    /**
+     * @see Table 18
+     *
+     * The object number of the object stream in which this object is
+     * stored. (The generation number of the object stream shall be
+     * implicitly 0.)
+     */
+    public final const GENERATION_NUMBER = 0;
+
     public function __construct(
         public readonly int $storedInObjectNumber,
         public readonly int $indexOfThisObjectWithinObjectStream,
