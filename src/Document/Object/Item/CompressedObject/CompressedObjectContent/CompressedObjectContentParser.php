052362d78310f2c73424eaf61cbe1ba4e04c9e87
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2024-11-17 13:47:11 +0100

    Use renamed DECODE_PARMS

--- src/Document/Object/Item/CompressedObject/CompressedObjectContent/CompressedObjectContentParser.php
+++ src/Document/Object/Item/CompressedObject/CompressedObjectContent/CompressedObjectContentParser.php
@@ -13,7 +13,7 @@ class CompressedObjectContentParser {
     public static function parse(Stream $stream, int $startPos, int $nrOfBytes, Dictionary $dictionary): string {
         $streamContent = $stream->read($startPos, $nrOfBytes);
         if (($streamFilter = $dictionary->getValueForKey(DictionaryKey::FILTER, FilterNameValue::class)) !== null) {
-            $streamContent = $streamFilter->decode($streamContent, $dictionary->getValueForKey(DictionaryKey::DECODE_PARAMS, ArrayValue::class));
+            $streamContent = $streamFilter->decode($streamContent, $dictionary->getValueForKey(DictionaryKey::DECODE_PARMS, ArrayValue::class));
         }
 
         return $streamContent;
