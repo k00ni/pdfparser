128779bc0ffdddcc53e2ab6cfecb17acff00f697
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-01-09 22:10:16 +0100

    Add manual test with non git-versioned samples for local testing on sample files

---
+++ tests/Manual/LocalSampleTest.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace PrinsFrank\PdfParser\Tests\Manual;
+
+use PHPUnit\Framework\Attributes\CoversNothing;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use PrinsFrank\PdfParser\Exception\RuntimeException;
+use PrinsFrank\PdfParser\PdfParser;
+
+#[CoversNothing]
+class LocalSampleTest extends TestCase {
+    #[DataProvider('samples')]
+    public function testGetText(string $path): void {
+        $document = (new PdfParser())
+            ->parseFile($path);
+
+        var_dump($document->getText());
+        $this->addToAssertionCount(1);
+    }
+
+    /** @return iterable<string, array{0: string}> */
+    public static function samples(): iterable {
+        $files = scandir($folder = __DIR__ . '/private-samples');
+        if ($files === false) {
+            throw new RuntimeException('Unable to retrieve files from samples-extra directory');
+        }
+
+        foreach ($files as $filePath) {
+            if (in_array($filePath, ['..', '.'], true)) {
+                continue;
+            }
+
+            yield $filePath => [$folder . '/' . $filePath];
+        }
+    }
+}
