9111e2353d0c2df0f55525ec74b48ea650591c8a
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-01-09 22:03:33 +0100

    getText and getTextObjectCollection can now rely on getObjectsByDictionaryKey method to support both reference values and reference value arrays

--- src/Document/Object/Decorator/Page.php
+++ src/Document/Object/Decorator/Page.php
@@ -14,20 +14,18 @@ use PrinsFrank\PdfParser\Exception\ParseFailureException;
 
 class Page extends DecoratedObject {
     public function getText(): string {
-        return $this->getTextObjectCollection()
-            ->getText($this->document, $this);
+        return implode(' ', array_map(
+            fn(TextObjectCollection $textObjectCollection) => $textObjectCollection->getText($this->document, $this),
+            $this->getTextObjectCollections(),
+        ));
     }
 
-    public function getTextObjectCollection(): TextObjectCollection {
-        $contentRef = $this->getDictionary()->getValueForKey(DictionaryKey::CONTENTS, ReferenceValue::class)
-            ?? throw new ParseFailureException(sprintf('No Contents found for page'));
-        $contentObject = $this->document->getObject($contentRef->objectNumber)
-            ?? throw new ParseFailureException(sprintf('Unable to locate content object with object number %d', $contentRef->objectNumber));
-        if (!$contentObject->objectItem instanceof UncompressedObject) {
-            throw new ParseFailureException('Compressed objects containing text are currently not supported');
-        }
-
-        return TextParser::parse($contentObject->objectItem->getStreamContent($this->document->stream));
+    /** @return list<TextObjectCollection> */
+    public function getTextObjectCollections(): array {
+        return array_map(
+            fn (DecoratedObject $decoratedObject) => TextParser::parse($decoratedObject->objectItem->getStreamContent($this->document->stream)),
+            $this->document->getObjectsByDictionaryKey($this->getDictionary(), DictionaryKey::CONTENTS),
+        );
     }
 
     public function getResourceDictionary(): ?Dictionary {
