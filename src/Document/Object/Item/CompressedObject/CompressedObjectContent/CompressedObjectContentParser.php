3dbcf6a9718b4f27217912c466be9d6ee7770db0
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-01-16 21:24:46 +0100

    Fix type safety in CompressedObjectContentParser

--- src/Document/Object/Item/CompressedObject/CompressedObjectContent/CompressedObjectContentParser.php
+++ src/Document/Object/Item/CompressedObject/CompressedObjectContent/CompressedObjectContentParser.php
@@ -7,6 +7,7 @@ use PrinsFrank\PdfParser\Document\Dictionary\Dictionary;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryKey\DictionaryKey;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Array\ArrayValue;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Name\FilterNameValue;
+use PrinsFrank\PdfParser\Exception\ParseFailureException;
 use PrinsFrank\PdfParser\Exception\RuntimeException;
 use PrinsFrank\PdfParser\Stream;
 
@@ -14,10 +15,14 @@ class CompressedObjectContentParser {
     public static function parse(Stream $stream, int $startPos, int $nrOfBytes, Dictionary $dictionary): string {
         $streamContent = $stream->read($startPos, $nrOfBytes);
         if (($filterType = $dictionary->getTypeForKey(DictionaryKey::FILTER)) === FilterNameValue::class) {
-            $streamContent = $dictionary->getValueForKey(DictionaryKey::FILTER, FilterNameValue::class)
+            $streamContent = ($dictionary->getValueForKey(DictionaryKey::FILTER, FilterNameValue::class) ?? throw new ParseFailureException())
                 ->decode($streamContent, $dictionary->getValueForKey(DictionaryKey::DECODE_PARMS, Dictionary::class));
         } elseif ($filterType === ArrayValue::class) {
-            foreach ($dictionary->getValueForKey(DictionaryKey::FILTER, ArrayValue::class)->value as $filterValue) {
+            foreach ($dictionary->getValueForKey(DictionaryKey::FILTER, ArrayValue::class)->value ?? throw new ParseFailureException() as $filterValue) {
+                if (is_string($filterValue) === false) {
+                    throw new ParseFailureException();
+                }
+
                 $streamContent = FilterNameValue::from(ltrim($filterValue, '/'))
                     ->decode($streamContent, $dictionary->getValueForKey(DictionaryKey::DECODE_PARMS, Dictionary::class));
             }
