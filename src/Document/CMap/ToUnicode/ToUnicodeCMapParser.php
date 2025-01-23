0932e0b6c262bcfb91c847ad5f32b00ddfaff84c
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-01-23 21:36:44 +0100

    Check bytesize before passing it to ToUnicodeCMap

--- src/Document/CMap/ToUnicode/ToUnicodeCMapParser.php
+++ src/Document/CMap/ToUnicode/ToUnicodeCMapParser.php
@@ -60,7 +60,7 @@ class ToUnicodeCMapParser {
         return new ToUnicodeCMap(
             (int) hexdec(trim($matchesSpaceRange['start'])),
             (int) hexdec(trim($matchesSpaceRange['end'])),
-            strlen(trim($matchesSpaceRange['start'])) / 2,
+            ($byteSize = (strlen(trim($matchesSpaceRange['start'])) / 2)) > 1 && is_int($byteSize) ? $byteSize : throw new ParseFailureException(),
             ...array_merge(...$bfCharRangeInfo)
         );
     }
