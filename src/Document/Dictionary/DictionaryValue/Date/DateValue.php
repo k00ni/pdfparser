a228d13b457e4f513761b065b23e942a3a886f98
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-01-24 22:00:02 +0100

    Properly accept hex timestamps

--- src/Document/Dictionary/DictionaryValue/Date/DateValue.php
+++ src/Document/Dictionary/DictionaryValue/Date/DateValue.php
@@ -6,6 +6,7 @@ namespace PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\Date;
 use DateTimeImmutable;
 use Override;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryValue\DictionaryValue;
+use PrinsFrank\PdfParser\Exception\ParseFailureException;
 
 class DateValue implements DictionaryValue {
     public function __construct(
@@ -15,14 +16,41 @@ class DateValue implements DictionaryValue {
 
     #[Override]
     public static function acceptsValue(string $value): bool {
-        return str_starts_with($value, '(D:') || str_starts_with($value, 'D:');
+        if (str_starts_with($value, '(D:') || str_starts_with($value, 'D:')) {
+            return true;
+        }
+
+        if (!str_starts_with($value, '<') || !str_ends_with($value, '>')) {
+            return false;
+        }
+
+        if (($decodedValue = hex2bin(substr($value, 1, -1))) === false) {
+            return false;
+        }
+
+        return str_starts_with(mb_convert_encoding($decodedValue, 'UTF-8', 'UTF-16'), 'D:')
+            || str_starts_with(mb_convert_encoding($decodedValue, 'UTF-8', 'UTF-16'), '(D:');
     }
 
     #[Override]
     public static function fromValue(string $valueString): self {
-        $parsedDate = DateTimeImmutable::createFromFormat('\(\D\:YmdHisP', str_replace("'", '', $valueString));
+        if (str_starts_with($valueString, '<') && str_ends_with($valueString, '>')) {
+            $valueString = substr($valueString, 1, -1);
+            $valueString = hex2bin($valueString);
+            if ($valueString === false) {
+                throw new ParseFailureException();
+            }
+
+            $valueString = mb_convert_encoding($valueString, 'UTF-8', 'UTF-16');
+        }
+
+        if (str_starts_with($valueString, '(')) {
+            $valueString = substr($valueString, 1);
+        }
+
+        $parsedDate = DateTimeImmutable::createFromFormat('\D\:YmdHisP', str_replace("'", '', $valueString));
         if ($parsedDate === false) {
-            return new self(null);
+            throw new ParseFailureException(sprintf('Unable to parse "%s" as a date value', $valueString));
         }
 
         return new self($parsedDate);
