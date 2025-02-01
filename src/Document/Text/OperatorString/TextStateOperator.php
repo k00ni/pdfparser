9714c5f393b1d19748076ed6c7c3947dd43827d0
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-02-01 17:04:50 +0100

    Font references can be valid dictionary keys

--- src/Document/Text/OperatorString/TextStateOperator.php
+++ src/Document/Text/OperatorString/TextStateOperator.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace PrinsFrank\PdfParser\Document\Text\OperatorString;
 
+use PrinsFrank\PdfParser\Document\Dictionary\DictionaryKey\DictionaryKey;
 use PrinsFrank\PdfParser\Document\Dictionary\DictionaryKey\ExtendedDictionaryKey;
 use PrinsFrank\PdfParser\Exception\InvalidArgumentException;
 
@@ -15,13 +16,17 @@ enum TextStateOperator: string {
     case RENDER = 'Tr';
     case RISE = 'Ts';
 
-    public function getFontReference(string $operand): ExtendedDictionaryKey {
+    public function getFontReference(string $operand): DictionaryKey|ExtendedDictionaryKey {
         if ($this !== self::FONT_SIZE) {
             throw new InvalidArgumentException('Can only retrieve font for Tf operator');
         }
 
         if (preg_match('/^\/(?<fontReference>[A-Za-z_0-9.]+)\s+[0-9]+(\.[0-9]+)?$/', $operand, $matches) !== 1) {
             throw new InvalidArgumentException(sprintf('Invalid font operand "%s"', substr($operand, 0, 200)));
+        }
+
+        if (($dictionaryKey = DictionaryKey::tryFrom($matches['fontReference'])) !== null) {
+            return $dictionaryKey;
         }
 
         return new ExtendedDictionaryKey($matches['fontReference']);
