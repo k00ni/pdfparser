58a9db7b6ad897a11dd25f2a10c819ed180a10b6
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-02-01 17:32:26 +0100

    Properly support bfrange and bfchar that are oneliners

--- src/Document/CMap/ToUnicode/ToUnicodeCMapParser.php
+++ src/Document/CMap/ToUnicode/ToUnicodeCMapParser.php
@@ -7,25 +7,27 @@ use PrinsFrank\PdfParser\Stream;
 
 class ToUnicodeCMapParser {
     public static function parse(Stream $stream, int $startOffset, int $nrOfBytes): ToUnicodeCMap {
-        $beginCodeSpaceRangePos = $stream->getStartNextLineAfter(ToUnicodeCMapOperator::BeginCodeSpaceRange, $startOffset, $startOffset + $nrOfBytes)
+        $beginCodeSpaceRangePos = $stream->firstPos(ToUnicodeCMapOperator::BeginCodeSpaceRange, $startOffset, $startOffset + $nrOfBytes)
             ?? throw new ParseFailureException(sprintf('Missing %s', ToUnicodeCMapOperator::BeginCodeSpaceRange->value));
+        $beginCodeSpaceRangePos += strlen(ToUnicodeCMapOperator::BeginCodeSpaceRange->value);
         $endCodeSpaceRangePos = $stream->firstPos(ToUnicodeCMapOperator::EndCodeSpaceRange, $beginCodeSpaceRangePos, $startOffset + $nrOfBytes)
             ?? throw new ParseFailureException();
         if (preg_match('/^\s*<(?P<start>[0-9a-fA-F ]+)>\s*<(?P<end>[0-9a-fA-F ]+)>\s*$/', $stream->read($beginCodeSpaceRangePos, $endCodeSpaceRangePos - $beginCodeSpaceRangePos), $matchesSpaceRange) !== 1) {
-            throw new ParseFailureException();
+            throw new ParseFailureException('Unrecognized codespacerange format');
         }
 
         if (strlen($matchesSpaceRange['start']) !== strlen($matchesSpaceRange['end'])) {
-            throw new ParseFailureException();
+            throw new ParseFailureException(sprintf('Start(%s) and end(%s) of codespacerange don\'t have the same number of bytes', $matchesSpaceRange['start'], $matchesSpaceRange['end']));
         }
 
         /** @var array<int, list<BFRange|BFChar>> $bfCharRangeInfo where the first index is used to track the position of the element in the CMap */
         $bfCharRangeInfo = [];
         $lastPos = $startOffset;
         while (($beginBFCharPos = $stream->firstPos(ToUnicodeCMapOperator::BeginBFChar, $lastPos, $startOffset + $nrOfBytes)) !== null) {
+            $beginBFCharPos += strlen(ToUnicodeCMapOperator::BeginBFChar->value);
             $endBFCharPos = $stream->firstPos(ToUnicodeCMapOperator::EndBFChar, $beginBFCharPos, $startOffset + $nrOfBytes)
                 ?? throw new ParseFailureException();
-            if (preg_match_all('/(*ANYCRLF)^\s*<(?P<start>[^>]+)>\s*<(?P<end>[^>]+)>\s*$/m', $stream->read($beginBFCharPos, $endBFCharPos - $beginBFCharPos), $matchesBFChar, PREG_SET_ORDER) === 0) {
+            if (preg_match_all('/\s*<(?P<start>[^>]+)>\s*<(?P<end>[^>]+)>\s*/', $stream->read($beginBFCharPos, $endBFCharPos - $beginBFCharPos), $matchesBFChar, PREG_SET_ORDER) === 0) {
                 throw new ParseFailureException('Unrecognized bfchar format');
             }
 
@@ -39,7 +41,7 @@ class ToUnicodeCMapParser {
         while (($beginBFRangePos = $stream->firstPos(ToUnicodeCMapOperator::BeginBFRange, $lastPos, $startOffset + $nrOfBytes)) !== null) {
             $endBFRangePos = $stream->firstPos(ToUnicodeCMapOperator::EndBFRange, $beginBFRangePos, $startOffset + $nrOfBytes)
                 ?? throw new ParseFailureException();
-            if (preg_match_all('/(*ANYCRLF)^\s*<(?P<start>[^>]+)>\s*<(?P<end>[^>]+)>\s*(?P<targetString>(<[^>]+>)|(\[\s*(<[^>]+>\s*)+\]))$/m', $stream->read($beginBFRangePos, $endBFRangePos - $beginBFRangePos), $matchesBFRange, PREG_SET_ORDER) === 0) {
+            if (preg_match_all('/\s*<(?P<start>[^>]+)>\s*<(?P<end>[^>]+)>\s*(?P<targetString>(<[^>]+>)|(\[\s*(<[^>]+>\s*)+\]))/', $stream->read($beginBFRangePos, $endBFRangePos - $beginBFRangePos), $matchesBFRange, PREG_SET_ORDER) === 0) {
                 throw new ParseFailureException('Unrecognized bfrange format');
             }
 
