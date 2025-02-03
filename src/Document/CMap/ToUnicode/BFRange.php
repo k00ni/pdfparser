ecba39084c4a0a7f636d76553bae6b211a9f688f
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-02-03 20:54:26 +0100

    Add support for destination characters that are surrogate pairs (#8)

--- src/Document/CMap/ToUnicode/BFRange.php
+++ src/Document/CMap/ToUnicode/BFRange.php
@@ -2,6 +2,7 @@
 
 namespace PrinsFrank\PdfParser\Document\CMap\ToUnicode;
 
+use PrinsFrank\PdfParser\Exception\ParseFailureException;
 use PrinsFrank\PdfParser\Exception\RuntimeException;
 
 class BFRange {
@@ -20,12 +21,29 @@ class BFRange {
 
     public function toUnicode(int $characterCode): ?string {
         if (count($this->destinationString) === 1) {
-            return mb_chr($this->destinationString[0] + $characterCode - $this->sourceCodeStart);
+            $destinationCodePoint = $this->destinationString[0] + $characterCode - $this->sourceCodeStart;
+        } else {
+            $destinationCodePoint = $this->destinationString[$characterCode - $this->sourceCodeStart]
+                ?? throw new RuntimeException(sprintf('Character code %d was not found in BFRange of length %d with start %d and end %d', $characterCode, count($this->destinationString), $this->sourceCodeStart, $this->sourceCodeEnd));
+        }
+
+        if ($destinationCodePoint <= 0x10FFFF) {
+            return mb_chr($destinationCodePoint);
         }
 
         return mb_chr(
-            $this->destinationString[$characterCode - $this->sourceCodeStart]
-                ?? throw new RuntimeException(sprintf('Character code %d was not found in BFRange of length %d with start %d and end %d', $characterCode, count($this->destinationString), $this->sourceCodeStart, $this->sourceCodeEnd))
+            $this->getCodePointForSurrogatePair($destinationCodePoint)
+                ?? throw new ParseFailureException('Destination code point is neither a valid unicode char nor a surrogate pair')
         );
+    }
+
+    private function getCodePointForSurrogatePair(int $surrogatePair): ?int {
+        $highSurrogate = ($surrogatePair >> 16) & 0xFFFF;
+        $lowSurrogate = $surrogatePair & 0xFFFF;
+        if ($highSurrogate < 0xD800 || $highSurrogate > 0xDBFF || $lowSurrogate < 0xDC00 || $lowSurrogate > 0xDFFF) {
+            return null;
+        }
+
+        return (($highSurrogate - 0xD800) << 10) + ($lowSurrogate - 0xDC00) + 0x10000;
     }
 }
