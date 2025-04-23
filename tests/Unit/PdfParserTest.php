36605dc48589cb43086e197171a3b25543d8bada
Konrad Abicht <abicht@mcon-consulting.de>
2025-04-23 14:33:21 +0200

    Demo case which triggers mentioned exception in https://github.com/PrinsFrank/pdfparser/issues/43

--- tests/Unit/PdfParserTest.php
+++ tests/Unit/PdfParserTest.php
@@ -55,6 +55,39 @@ class PdfParserTest extends TestCase {
         );
     }
 
+    /**
+     * @see https://github.com/PrinsFrank/pdfparser/issues/43
+     */
+    public function testIssue43(): void {
+        /*
+         * Running the following code should lead to the exception:
+         *
+         *      PrinsFrank\PdfParser\Exception\ParseFailureException: Value "/Viewport" for dictionary key Type could not be parsed to a valid value type
+         *
+         * Stack trace:
+         *
+         * /var/www/html/src/Document/Dictionary/DictionaryEntry/DictionaryEntryFactory.php:75
+         * /var/www/html/src/Document/Dictionary/DictionaryEntry/DictionaryEntryFactory.php:29
+         * /var/www/html/src/Document/Dictionary/DictionaryFactory.php:24
+         * /var/www/html/src/Document/Dictionary/DictionaryParser.php:83
+         * /var/www/html/src/Document/Object/Item/UncompressedObject/UncompressedObject.php:49
+         * /var/www/html/src/Document/Object/Decorator/DecoratedObjectFactory.php:22
+         * /var/www/html/src/Document/Document.php:112
+         * /var/www/html/src/Document/Object/Decorator/Pages.php:19
+         * /var/www/html/src/Document/Document.php:132
+         * /var/www/html/src/Document/Document.php:141
+         * /var/www/html/tests/Unit/PdfParserTest.php:80
+         */
+
+        $parser = new PdfParser();
+        static::assertSame(
+            '',
+            $parser
+                ->parseFile(__DIR__ . '/mbl-2025-16.pdf')
+                ->getText(PHP_EOL)
+        );
+    }
+
     public function testParseFileThrowsExceptionWhenUnableToOpenFile(): void {
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Failed to open file at path "oeuoeu"');
